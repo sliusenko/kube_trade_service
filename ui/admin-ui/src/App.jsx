ccc54fa83c938e45b23c7994810f20e56b367e5a
Oleksandr Sliusenko <134318414+sliusenko@users.noreply.github.com>
2025-09-21 14:29:48 +0300

    Create App.jsx

---
+++ ui/admin-ui/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{padding:16}}>
+      <h2>Admin UI</h2>
+      <p>Here will live admin dashboards, config editors, users & roles, etc.</p>
+    </div>
+  );
+}
