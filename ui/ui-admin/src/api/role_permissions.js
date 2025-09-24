36528be97da0e5a7974c1e4b610a95c6870613c0
sliusenko <a.slusenko@gmail.com>
2025-09-24 20:58:53 +0300

    nev pr

--- ui/ui-admin/src/api/role_permissions.js
+++ ui/ui-admin/src/api/role_permissions.js
@@ -2,18 +2,18 @@ import apiClient from "./apiClient";
 
 // отримати всі бінди (role ↔ permissions)
 export const getRolePermissions = async () => {
-  const response = await apiClient.get("/roles/bind");
+  const response = await apiClient.get("/role-permissions");
   return response.data;
 };
 
 // додати прив'язку роль ↔ permission
 export const createRolePermission = async (payload) => {
-  const response = await apiClient.post("/roles/bind", payload);
+  const response = await apiClient.post("/role-permissions", payload);
   return response.data;
 };
 
 // видалити прив'язку роль ↔ permission
 export const deleteRolePermission = async (roleName, permissionName) => {
-  const response = await apiClient.delete(`/roles/bind/${roleName}/${permissionName}`);
+  const response = await apiClient.delete(`/role-permissions/${roleName}/${permissionName}`);
   return response.data;
 };
