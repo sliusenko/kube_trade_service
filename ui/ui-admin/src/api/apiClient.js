e59ebcc743dbd4a7d701ca78a23e9ac65f1d58a7
Oleksandr Sliusenko <134318414+sliusenko@users.noreply.github.com>
2025-09-28 15:41:39 +0300

    Update apiClient.js

--- ui/ui-admin/src/api/apiClient.js
+++ ui/ui-admin/src/api/apiClient.js
@@ -1,8 +1,20 @@
-// src/api/apiClient.js
+// apiClient.js
 import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: "/core-admin",
+  withCredentials: true,
 });
+
+apiClient.interceptors.response.use(
+  (response) => {
+
+    if (response.request?.responseURL?.includes("/oauth2/start")) {
+      return Promise.reject(new Error("Not authenticated"));
+    }
+    return response;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default apiClient;
