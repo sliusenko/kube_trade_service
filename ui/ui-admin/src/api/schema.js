c0e1b6fafe188d1b1bd093fa77f4477d06a4ccc7
Oleksandr Sliusenko <134318414+sliusenko@users.noreply.github.com>
2025-09-28 15:49:13 +0300

    Update schema.js

--- ui/ui-admin/src/api/schema.js
+++ ui/ui-admin/src/api/schema.js
@@ -1,7 +1,5 @@
+// src/api/schema.js
 import apiClient from "./apiClient";
 
-// Отримати OpenAPI-схему (всі моделі)
-export const getSchema = async () => {
-  const res = await apiClient.get("/openapi.json");
-  return res.data;
-};
+export const getSchema = () =>
+  apiClient.get("/openapi.json").then((r) => r.data);
