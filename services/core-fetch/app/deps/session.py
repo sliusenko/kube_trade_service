0160343b31459d3257030bf0cfface0453a2dd9e
Oleksandr Sliusenko <134318414+sliusenko@users.noreply.github.com>
2025-09-29 10:40:44 +0300

    Update session.py

--- services/core-fetch/app/deps/session.py
+++ services/core-fetch/app/deps/session.py
@@ -3,3 +3,7 @@ from app.deps.config import settings
 
 engine = create_async_engine(settings.POSTGRES_DSN, echo=False, future=True)
 SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+
+async def get_session() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        yield session
