9d2691b98f944402fdfded5465acb9071b2d9bb0
Oleksandr Sliusenko <134318414+sliusenko@users.noreply.github.com>
2025-09-29 09:28:46 +0300

    Create db.py

---
+++ services/core-news/app/deps/db.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+import os
+
+DB_CONFIG = {
+    "dbname": os.getenv("DB_NAME", "trade"),
+    "user": os.getenv("DB_USER", "postgres"),
+    "password": os.getenv("DB_PASSWORD", ""),
+    "host": os.getenv("DB_HOST", "postgres"),
+    "port": os.getenv("DB_PORT", 5432),
+}
+
+DATABASE_URL = (
+    f"postgresql+asyncpg://{DB_CONFIG['user']}:{DB_CONFIG['password']}"
+    f"@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['dbname']}"
+)
+
+engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+# Dependency for FastAPI
+async def get_session() -> AsyncSession:
+    async with async_session_maker() as session:
+        yield session
