b60c1dccb7cd550e1da9577cc2764f3d3706fd28
Oleksandr Sliusenko <134318414+sliusenko@users.noreply.github.com>
2025-09-29 16:45:08 +0300

    Update users.py

--- services/common/schemas/users.py
+++ services/common/schemas/users.py
@@ -30,3 +30,23 @@ class UserOut(UserBase):
 
     class Config:
         from_attributes = True
+
+# -----------------------------
+# Permission
+# -----------------------------
+class PermissionBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+
+class PermissionCreate(PermissionBase):
+    pass
+
+
+class PermissionUpdate(BaseModel):
+    description: Optional[str]
+
+
+class PermissionOut(PermissionBase):
+    class Config:
+        from_attributes = True
