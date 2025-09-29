2e9d3b66a434f99827d72d8a06b73d3ef880edf5
Oleksandr Sliusenko <134318414+sliusenko@users.noreply.github.com>
2025-09-29 09:34:42 +0300

    Update main.py

--- services/core-news/app/main.py
+++ services/core-news/app/main.py
@@ -2,16 +2,25 @@ from fastapi import FastAPI
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from app.routers import news
-from app.services.news_service import check_news_and_halt_trading, update_news_prices
+from app.services import news_service
+from app.deps.db import async_session_maker
 
 app = FastAPI(title="core-news")
 
-# Include routes
 app.include_router(news.router)
 
 @app.on_event("startup")
 async def startup_event():
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(check_news_and_halt_trading, "interval", minutes=10)
-    scheduler.add_job(update_news_prices, "interval", hours=1)
+
+    async def job_check_news():
+        async with async_session_maker() as session:
+            await news_service.check_news_and_halt_trading(session)
+
+    async def job_update_prices():
+        async with async_session_maker() as session:
+            await news_service.update_news_prices(session)
+
+    scheduler.add_job(job_check_news, "interval", minutes=10)
+    scheduler.add_job(job_update_prices, "interval", hours=1)
     scheduler.start()
