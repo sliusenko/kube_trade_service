2fee9219633e87342a420a419adbd4fcef569042
Oleksandr Sliusenko <134318414+sliusenko@users.noreply.github.com>
2025-09-30 08:32:21 +0300

    Update scheduler.py

--- services/common/schemas/scheduler.py
+++ services/common/schemas/scheduler.py
@@ -1,10 +1,14 @@
-from pydantic import BaseModel
+# app/schemas/apscheduler_job.py
 from typing import Optional
+from pydantic import BaseModel, ConfigDict
 
-class JobOut(BaseModel):
+
+class ApschedulerJobBase(BaseModel):
     id: str
     next_run_time: Optional[float] = None
-    job_state: bytes
+class ApschedulerJobCreate(ApschedulerJobBase):
+    job_state: bytes  # у create можна приймати raw state
+class ApschedulerJobOut(ApschedulerJobBase):
+    model_config = ConfigDict(from_attributes=True)
 
-    class Config:
-        from_attributes = True
+    job_state: bytes
