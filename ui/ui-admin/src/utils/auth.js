48818f15412e56825d63a47a237b71836d297aca
Oleksandr Sliusenko <134318414+sliusenko@users.noreply.github.com>
2025-09-30 13:32:10 +0300

    Update auth.js

--- ui/ui-admin/src/utils/auth.js
+++ ui/ui-admin/src/utils/auth.js
@@ -1,12 +1,12 @@
 // src/utils/auth.js
 export function signOut() {
-  const keycloakUrl = process.env.REACT_APP_KEYCLOAK_URL;
-  const realm = process.env.REACT_APP_KEYCLOAK_REALM;
-  const portalUrl = process.env.REACT_APP_PORTAL_URL;
+  const keycloakUrl = window._env_.REACT_APP_KEYCLOAK_URL;
+  const realm = window._env_.REACT_APP_KEYCLOAK_REALM;
+  const portalUrl = window._env_.REACT_APP_PORTAL_URL || window.location.origin;
 
   const logoutUrl =
-    `${keycloakUrl}/realms/${realm}/protocol/openid-connect/logout` +
-    `?redirect_uri=${encodeURIComponent(portalUrl)}`;
+    `${keycloakUrl}/realms/${realm}/protocol/openid-connect/logout?` +
+    `redirect_uri=${encodeURIComponent(portalUrl)}`;
 
   window.location.href = logoutUrl;
 }
