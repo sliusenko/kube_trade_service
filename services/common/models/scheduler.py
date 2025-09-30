c1db45190dbef4fb45efa87e2a8de38fd2341848
Oleksandr Sliusenko <134318414+sliusenko@users.noreply.github.com>
2025-09-30 08:31:56 +0300

    Update scheduler.py

--- services/common/models/scheduler.py
+++ services/common/models/scheduler.py
@@ -1,9 +1,16 @@
-from sqlalchemy import Column, String, Float, LargeBinary
+# app/models/apscheduler_job.py
+from typing import Optional
+from sqlalchemy import String, Float, LargeBinary
+from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
+
 
 class ApschedulerJob(Base):
     __tablename__ = "apscheduler_jobs"
 
-    id = Column(String(191), primary_key=True)
-    next_run_time = Column(Float, nullable=True)
-    job_state = Column(LargeBinary, nullable=False)
+    id: Mapped[str] = mapped_column(String(191), primary_key=True)
+    next_run_time: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+    job_state: Mapped[bytes] = mapped_column(LargeBinary, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<ApschedulerJob id={self.id} next_run_time={self.next_run_time}>"
