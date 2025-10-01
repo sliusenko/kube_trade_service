99da3aa3f1fd68160caa14a490efa58611c75a48
sliusenko <a.slusenko@gmail.com>
2025-10-01 22:20:04 +0300

    nev pr

--- services/core_admin/app/main.py
+++ services/core_admin/app/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 # from app.deps.db import engine
 from core_admin.app.routers import (
     users, roles, permissions,
-    role_permissions, scheduler, exchanges, news, dashboard
+    role_permissions, scheduler, exchanges, news, dashboard, config
 )
 
 app = FastAPI(title="Admin-core API")
@@ -24,6 +24,7 @@ app.include_router(scheduler.router)
 app.include_router(exchanges.router)
 app.include_router(news.router)
 app.include_router(dashboard.router)
+app.include_router(config.router)
 
 @app.get("/")
 async def root():
