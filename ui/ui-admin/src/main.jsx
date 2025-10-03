11384add9db06699c72a858ad04089ff7aa2c314
sliusenko <a.slusenko@gmail.com>
2025-10-03 17:49:02 +0300

    nev pr

--- ui/ui-admin/src/main.jsx
+++ ui/ui-admin/src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/admin">
+    <App />
+  </BrowserRouter>
 );
